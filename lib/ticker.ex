d0df22bbed14901dfd040a1586cd7d12d6cbaa45
Mike Hoffert <mike.hoffert@gmail.com>
2016-12-28 14:33:46 -0700

    Ticker: interval timer for scheduling tasks

--- lib/ticker.ex
+++ lib/ticker.ex
@@ -2,4 +2,23 @@ defmodule Ticker do
   @moduledoc """
   Timer to schedule jobs in small intervals.
   """
+
+  def init(state) do
+    send(self, :tick)
+    {:ok, state}
+  end
+
+  def handle_info(:tick, state) do
+    schedule_tick(state)
+    work(state)
+    {:noreply, state}
+  end
+
+  defp work(%{module: module, function: function}) do
+    apply(module, function, [])
+  end
+
+  defp schedule_tick(%{interval: interval}) do
+    Process.send_after(self, :tick, interval)
+  end
 end
--- mix.exs
+++ mix.exs
@@ -4,11 +4,14 @@ defmodule Ticker.Mixfile do
   def project do
     [
       app: :ticker,
-      version: "0.0.1",
+      version: "0.0.2",
       elixir: "~> 1.3",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package(),
+      description: description(),
+      docs: [extras: ["README.md"]]
     ]
   end
 
@@ -21,6 +24,22 @@ defmodule Ticker.Mixfile do
   defp deps do
     [
       {:credo, "~> 0.4.12", only: [:dev, :test]},
+      {:ex_doc, "~> 0.13", only: [:dev, :test]}
     ]
+  end
+
+  defp package do
+    [
+      name: :ticker,
+      files: ["lib"],
+      maintainers: ["OnBoardingSystems"],
+      licenses: ["Apache 2.0"],
+      links: %{"GitHub" => "https://github.com/onboardingsystems/ticker"}
+    ]
+  end
+  defp description do
+    """
+    Timer to schedule jobs in small intervals.
+    """
   end
 end
